5f411ef57351896ada53b1823479320391c6fb25
yagyeshverma <123866273+yagyeshverma@users.noreply.github.com>
2024-09-26 00:17:51 +0530

    Add files via upload

---
+++ Tribute-page/script.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+// Next/previous controls
+function plusSlides(n) {
+    slideIndex += n - 1; // Adjust for automatic increment
+    showSlides();
+}
